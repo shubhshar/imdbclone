d25400014a9ea9aa2b0fcfb0eec9cfe0c03bb2a2
Shubham <49046719+shubhs10@users.noreply.github.com>
2022-10-13 02:08:39 +0530

    Added popular,toprated,upcoming and cards pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Homepage from "./pages/Homepage";
+import Movielist from "./Components/MovieList/Movielist";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Homepage />}></Route>
           <Route path="movie/:id" element={<h1>Id page</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Type page</h1>}></Route>
+          <Route path="movies/:type" element={<Movielist />}></Route>
           <Route path="/*" element={<h1>Error</h1>}></Route>
         </Routes>
       </Router>
